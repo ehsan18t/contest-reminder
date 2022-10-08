00655295ffd28a6a1969aac8dee8f7caa7df1d9c
Ahsan Khan <mkhan201122@bscse.uiu.ac.bd>
2022-10-09 00:58:45 +0600

    added email sender

---
+++ src/main/java/dev/pages/ahsan/creminder/email/MailSender.java
@@ -0,0 +1,54 @@
+package dev.pages.ahsan.creminder.email;
+
+import dev.pages.ahsan.creminder.main.Config;
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+import java.util.Properties;
+
+public class MailSender {
+    //this is responsible to send email.
+    public static void sendEmail(String email, String pass, String message, String subject, String to) {
+        //get the system properties
+        Properties properties = System.getProperties();
+
+        //host config
+//        properties.put("mail.smtp.starttls.enable","true");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.host", Config.HOST);
+        properties.put("mail.smtp.port", Config.PORT);
+
+        //Step 1: to get the session object.
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(email, pass);
+            }
+        });
+//        session.setDebug(true);
+
+        MimeMessage m = new MimeMessage(session);
+        try {
+            // from email
+            m.setFrom(email);
+
+            // adding recipient to message
+            m.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // adding subject to message
+            m.setSubject(subject);
+
+            // adding text to message
+            m.setText(message);
+
+            // send
+            Transport.send(m);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
--- src/main/java/dev/pages/ahsan/creminder/main/Config.java
+++ src/main/java/dev/pages/ahsan/creminder/main/Config.java
@@ -1,5 +1,10 @@
 package dev.pages.ahsan.creminder.main;
 
 public class Config {
+    // APIs
     public static String contestAPI = "https://codeforces.com/api/contest.list?gym=";
+
+    // Mail Server Settings
+    public static final String HOST = "smtp.gmail.com";
+    public static final String PORT = "465";
 }
