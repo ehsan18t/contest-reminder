b1fe57b88078498de697eb26e27ad9ee7b6f9023
Ehsan Khan <ehsan18t@gmail.com>
2023-01-19 00:19:34 +0600

    added main api parser

---
+++ scraper/MainAPIParser.py
@@ -0,0 +1,10 @@
+import json
+import requests
+
+
+class MainAPIParser:
+    def __init__(self, url):
+        self.url = url
+
+    def fetch(self):
+        return json.loads(requests.get(self.url).content)
