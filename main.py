e5c87ec3935ed51bac06858eea0195fa68985c5e
Ehsan Khan <ehsan18t@gmail.com>
2023-01-19 09:56:21 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from scraper.CodeChef import CodeChef
 from scraper.CodeForces import CodeForces
 
 if __name__ == '__main__':
+    # CodeForces Example
     cf = CodeForces()
     cf.init()
     for ll in cf.getContests():
         print(ll)
+    # CodeChef Example
+    cc = CodeChef()
+    for c in cc.fetch()['future_contests']:
+        print(c['contest_name'])
