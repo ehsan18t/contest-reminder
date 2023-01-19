d3440fd54b449c8b3a4c6e40734449f9c30b14f5
Ehsan Khan <ehsan18t@gmail.com>
2023-01-19 21:22:12 +0600

    added time-date zone conversion

--- scraper/CodeForces.py
+++ scraper/CodeForces.py
@@ -1,13 +1,15 @@
-from scraper.MainScraper import MainScraper
+from scraper.MainScraper import MainScraper, parseDateTime
 
 
 class CodeForces(MainScraper):
     url = 'https://codeforces.com/contests'
+    ojZone = 'Europe/Moscow'
 
     def __init__(self):
         super().__init__(self.url)
 
     def getContests(self):
+        self.init()
         contests = []
         table = self.soup.find('table')
         rows = table.findAll('tr')[1:]  # Excluding Table header
@@ -15,9 +17,11 @@ class CodeForces(MainScraper):
         for row in rows:
             contest = []
             row = row.findAll('td')
-            row.pop(1)     # Excluding Writers Name
+            row.pop(1)  # Excluding Writers Name
             for td in row:
                 contest.append(td.get_text().strip())
+            contest[1] = parseDateTime(contest[1], self.ojZone, '%b/%d/%Y %H:%M')
+            contest[1] = contest[1].strftime('%Y-%m-%d %H:%M')
             contests.append(contest)
 
         return contests
--- scraper/MainScraper.py
+++ scraper/MainScraper.py
@@ -1,9 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime
+from pytz import timezone
+from config import Config
+
+
+def parseDateTime(dt, dtzone, fmt):
+    tz1 = timezone(dtzone)
+    tz2 = timezone(Config.zoneID)
+
+    new_dt = datetime.datetime.strptime(dt, fmt)
+    new_dt = tz1.localize(new_dt)
+
+    # Convert zone
+    return new_dt.astimezone(tz2)
 
 
 class MainScraper:
     soup = None
+    month = {
+        'Jan': 1,
+        'Feb': 2,
+        'Mar': 3,
+        'Apr': 4,
+        'May': 5,
+        'Jun': 6,
+        'Jul': 7,
+        'Aug': 8,
+        'Sep': 9,
+        'Oct': 10,
+        'Nov': 11,
+        'Dec': 12
+    }
 
     def __init__(self, url):
         self.url = url
